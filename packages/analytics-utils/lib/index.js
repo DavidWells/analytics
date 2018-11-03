fdeb37cfbfed1003c0b853fa0e3822eebb49c964
DavidWells <hello@davidwells.io>
2018-11-03 14:22:28 -0700

    chore(utils): update build

--- packages/analytics-utils/lib/index.js
+++ packages/analytics-utils/lib/index.js
@@ -1,12 +1,15 @@
 // @create-index
 
+export { default as cookie } from './cookie.js';
 export { default as decodeUri } from './decodeUri.js';
 export { default as extend } from './extend.js';
 export { default as inBrowser } from './inBrowser.js';
 export { default as isExternalReferrer } from './isExternalReferrer.js';
+export { default as noOp } from './noOp.js';
 export { default as paramsClean } from './paramsClean.js';
 export { default as paramsGet } from './paramsGet.js';
 export { default as paramsParse } from './paramsParse.js';
 export { default as paramsRemove } from './paramsRemove.js';
+export { default as storage } from './storage';
 export { default as uuid } from './uuid.js';
 
