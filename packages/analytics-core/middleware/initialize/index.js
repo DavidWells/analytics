44d05d53cc189f3c177a57c3e976d5757ac9f2d2
DavidWells <hello@davidwells.io>
2018-11-04 23:56:39 -0800

    feat(core): add querystring API for triggering events + identify calls via utm params

--- packages/analytics-core/middleware/initialize/index.js
+++ packages/analytics-core/middleware/initialize/index.js
@@ -1,5 +1,7 @@
 import EVENTS from '../../events'
-import { paramsParse } from 'analytics-utils'
+import { identify } from '../../modules/user'
+import { trackEvent } from '../../modules/track'
+import { paramsParse, paramsRemove } from 'analytics-utils'
 
 // Middleware runs during EVENTS.INITIALIZE
 const initializeMiddleware = store => next => action => {
@@ -9,23 +11,64 @@ const initializeMiddleware = store => next => action => {
     // 2. Parse params
 
     const params = paramsParse()
-    if (params) {
+    const paramsArray = Object.keys(params)
+    if (paramsArray.length) {
       store.dispatch({
         type: EVENTS.HANDLE_PARAMS,
         params: params
       })
 
-      const utmParams = Object.keys(params).reduce((arr, key) => {
-        if (key.match(/utm_/)) arr[key] = params[key]
-        return arr
-      }, {})
+      // parse params
+      const groupedParams = paramsArray.reduce((acc, key) => {
+        if (key.match(/^utm_/)) {
+          acc.utm[key] = params[key]
+        }
+        if (key.match(/^an_prop_/)) {
+          acc.props[`${key.replace(/^an_prop_/, '')}`] = params[key]
+        }
+        if (key.match(/^an_trait_/)) {
+          acc.traits[`${key.replace(/^an_trait_/, '')}`] = params[key]
+        }
+        return acc
+      }, {
+        utm: {},
+        props: {},
+        traits: {}
+      })
+
+      if (params.an_uid) {
+        console.log('DO identity call', params.an_uid)
+        // timeout to debounce and make sure integration is registered. Todo refactor
+        setTimeout(() => {
+          // Dispatch identify call
+          store.dispatch(identify(params.an_uid, groupedParams.traits))
+        }, 0)
+      }
+
+      if (params.an_event) {
+        // timeout to debounce and make sure integration is registered. Todo refactor
+        setTimeout(() => {
+          store.dispatch(
+            // Dispatch track call
+            trackEvent(params.an_event, groupedParams.props)
+          )
+        }, 0)
+      }
 
       // if url has utm params
-      if (Object.keys(utmParams).length) {
+      if (Object.keys(groupedParams.utm).length) {
         store.dispatch({
           type: EVENTS.SET_CAMPAIGN,
-          campaign: utmParams
+          campaign: groupedParams.utm
         })
+      }
+
+      if (params.an_clean) {
+        console.log('RUN CLEAN')
+        // timeout to debounce and make sure integration is registered. Todo refactor
+        setTimeout(() => {
+          paramsRemove('an_')
+        }, 0)
       }
     }
     // handle user indenfication here
