0752524ad4a388f8b0236a1e07442679e194103a
DavidWells <hello@davidwells.io>
2018-12-02 14:57:35 -0800

    docs(utils): add decode docblock

--- packages/analytics-utils/lib/decodeUri.js
+++ packages/analytics-utils/lib/decodeUri.js
@@ -1,7 +1,11 @@
-
-/*
-	Input > "Bought%20keyword
-	Output > "Bought keyword"
+/**
+ * Decode URI string
+ *
+ * @param {String} s string to decode
+ * @returns {String} decoded string
+ * @example
+ * decode("Bought%20keyword)
+ * => "Bought keyword"
  */
 export default function decode(s) {
   return decodeURIComponent(s).replace(/\+/g, ' ')
