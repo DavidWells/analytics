fa7eaecfbc813d72276058a248574d58d8f3bc67
DavidWells <hello@davidwells.io>
2018-12-02 18:59:09 -0800

    refactor(core): update filter disabled into single util

--- packages/analytics-core/utils/filterDisabled.js
+++ packages/analytics-core/utils/filterDisabled.js
@@ -1,8 +1,14 @@
 
-export default function fitlerDisabled(methodCalls, options) {
-  return methodCalls.filter((provider) => {
-    const integrations = options && options.integrations
-    const disabled = integrations && integrations[provider.NAMESPACE] === false
-    return !disabled
+export default function fitlerDisabled(methodCalls, settings = {}, options = {}) {
+  return methodCalls.filter((current) => {
+    const name = current.NAMESPACE
+    const { integrations } = options
+    // If enabled by options. Overide settings
+    if (integrations && integrations[name] && integrations[name] === true) {
+      return true
+    }
+    const disabledBySettings = settings[name] && settings[name].enabled === false
+    const disabledByOptions = integrations && integrations[name] === false
+    return !disabledBySettings && !disabledByOptions
   })
 }
