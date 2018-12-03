b2e06b6c773cb76454b511b24a133f5da92a0c56
DavidWells <hello@davidwells.io>
2018-12-02 19:00:23 -0800

    feat(core): add network events

---
+++ packages/analytics-core/utils/throttle.js
@@ -0,0 +1,26 @@
+export default function throttle(func, wait) {
+  var context, args, result
+  var timeout = null
+  var previous = 0
+  var later = function() {
+    previous = new Date()
+    timeout = null
+    result = func.apply(context, args)
+  }
+  return function() {
+    var now = new Date()
+    if (!previous) previous = now
+    var remaining = wait - (now - previous)
+    context = this
+    args = arguments
+    if (remaining <= 0) {
+      clearTimeout(timeout)
+      timeout = null
+      previous = now
+      result = func.apply(context, args)
+    } else if (!timeout) {
+      timeout = setTimeout(later, remaining)
+    }
+    return result
+  }
+}
