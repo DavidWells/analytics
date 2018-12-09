174805d20917287ef9e618c6e914411002860acc
DavidWells <hello@davidwells.io>
2018-12-08 17:18:08 -0800

    feat(utils): add clientside url parsing utils

---
+++ packages/analytics-utils/lib/url.js
@@ -0,0 +1,44 @@
+import inBrowser from './inBrowser'
+
+/**
+ * Get host domain of url
+ * @param  {String} url - href of page
+ * @return {String} hostname of page
+ *
+ * @example
+ *  getDomainHost('https://subdomain.my-site.com/')
+ *  > subdomain.my-site.com
+ */
+export function getDomainHost(url) {
+  if (!inBrowser) return null
+  const a = document.createElement('a')
+  a.setAttribute('href', url)
+  return a.hostname
+}
+
+/**
+ * Get host domain of url
+ * @param  {String} url - href of page
+ * @return {String} base hostname of page
+ *
+ * @example
+ *  getDomainHost('https://subdomain.my-site.com/')
+ *  > my-site.com
+ */
+export function getDomainBase(url) {
+  const host = getDomainHost(url) || ''
+  return host.split('.').slice(-2).join('.')
+}
+
+/**
+ * Remove TLD from domain string
+ * @param  {String} baseDomain - host name of site
+ * @return {String}
+ * @example
+ *  trimTld('google.com')
+ *  > google
+ */
+export function trimTld(baseDomain) {
+  const arr = baseDomain.split('.')
+  return (arr.length > 1) ? arr.slice(0, -1).join('.') : baseDomain
+}
