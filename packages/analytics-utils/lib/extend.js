4b13eff71e78eee87e7ddeac22ebaf41186ed361
DavidWells <hello@davidwells.io>
2019-01-20 17:47:26 -0800

    refactor(utils): remove extend utilities. extending plugins should happen in user land

--- packages/analytics-utils/lib/extend.js
+++
@@ -1,8 +0,0 @@
-
-// Allows for userland overrides of base functions
-export default function extend(methodName, defaultFunction, config) {
-  if (config[methodName] && typeof config[methodName] === 'function') {
-    return config[methodName]
-  }
-  return defaultFunction
-}
