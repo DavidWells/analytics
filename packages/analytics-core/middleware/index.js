4a5e6aee9f6ce67effa485ab90cb8682e6ec6f1f
DavidWells <hello@davidwells.io>
2019-01-22 22:08:20 -0800

    refactor(core): move track & page middleware

--- packages/analytics-core/middleware/index.js
+++ packages/analytics-core/middleware/index.js
@@ -1,7 +1,5 @@
 
 export { default as initialize } from './initialize'
-export { default as page } from './page'
-export { default as track } from './track'
 export { default as identify } from './identify'
 export { default as plugins } from './plugins'
 export { default as storage, setItem, removeItem, getItem } from './storage'
