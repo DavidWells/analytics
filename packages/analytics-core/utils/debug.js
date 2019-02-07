daaef87d8eb0e62f9adde76331dcc359655b570d
DavidWells <hello@davidwells.io>
2019-02-06 23:11:45 -0800

    feat(core): add debug utilities

---
+++ packages/analytics-core/utils/debug.js
@@ -0,0 +1,24 @@
+import { compose } from 'redux'
+
+export function Debug() {
+  const config = arguments[0] || {} // eslint-disable-line
+  // initialize global history
+  if (process.browser) window.__ANALYTICS_HISTORY__ = []
+  return (createStore) => {
+    return (reducer, preloadedState, enhancer) => {
+      const store = createStore(reducer, preloadedState, enhancer)
+      const origDispatch = store.dispatch
+      const dispatch = (action) => {
+        window.__ANALYTICS_HISTORY__.push(action.action)
+        return origDispatch(action)
+      }
+      return Object.assign(store, { dispatch: dispatch })
+    }
+  }
+}
+
+export function composeWithDebug(config) {
+  return function () {
+    return compose(compose.apply(null, arguments), Debug(config))
+  }
+}
