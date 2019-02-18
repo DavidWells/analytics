a6cb75a0d6f5ce796c6e283ceecdef82a9f02a05
DavidWells <hello@davidwells.io>
2019-02-17 23:59:25 -0800

    refactor(listener middleware): scope middleware chain to support multiple instances

--- packages/analytics-core/src/middleware/dynamic.js
+++ packages/analytics-core/src/middleware/dynamic.js
@@ -1,53 +1,38 @@
-/* forked from https://github.com/pofigizm/redux-dynamic-middlewares/ */
 import { compose } from 'redux'
 
-let before = []
-let after = []
+/* Class to fix dynamic middlewares from conflicting with each other
+if more than one analytic instance is active */
+export default class DynamicMiddleware {
+  before = []
+  after = []
+  addMiddleware = (middlewares, position) => {
+    this[position] = this[position].concat(middlewares)
+  }
+  removeMiddleware = (middleware, position) => {
+    const index = this[position].findIndex(d => d === middleware)
+    if (index === -1) return
 
-const dynamicMiddlewares = (positionBefore) => {
-  return store => next => action => {
-    const middlewareAPI = {
-      getState: store.getState,
-      dispatch: (act) => store.dispatch(act),
+    this[position] = [
+      ...this[position].slice(0, index),
+      ...this[position].slice(index + 1),
+    ]
+  }
+  resetMiddlewares = (position) => {
+    if (!position) {
+      this.before = []
+      this.after = []
+    } else {
+      this[position] = []
     }
-    const middlewareChain = (positionBefore) ? before : after
-    const chain = middlewareChain.map(middleware => middleware(middlewareAPI))
-    return compose(...chain)(next)(action)
+  }
+  dynamicMiddlewares = (position) => {
+    return store => next => action => {
+      const middlewareAPI = {
+        getState: store.getState,
+        dispatch: (act) => store.dispatch(act),
+      }
+      const chain = this[position].map(middleware => middleware(middlewareAPI))
+      return compose(...chain)(next)(action)
+    }
   }
 }
-
-export const addMiddleware = (middlewares, positionBefore) => {
-  if (positionBefore) {
-    before = before.concat(middlewares)
-  } else {
-    after = after.concat(middlewares)
-  }
-}
-
-export const removeMiddleware = (middleware, positionBefore) => {
-  const middlewareChain = (positionBefore) ? before : after
-  const index = middlewareChain.findIndex(d => d === middleware)
-  if (index === -1) return
-
-  if (positionBefore) {
-    before = [
-      ...before.slice(0, index),
-      ...before.slice(index + 1),
-    ]
-  } else {
-    after = [
-      ...after.slice(0, index),
-      ...after.slice(index + 1),
-    ]
-  }
-}
-
-export const newresetMiddlewares = (positionBefore) => {
-  if (positionBefore) {
-    before = []
-  } else {
-    after = []
-  }
-}
-
-export default dynamicMiddlewares
