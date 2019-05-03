9d21b4b815b3b8424c04b442246240eeb8ea1597
DavidWells <hello@davidwells.io>
2019-05-02 21:18:48 -0700

    refactor(core): support debugger in node

--- packages/analytics-core/src/utils/debug.js
+++ packages/analytics-core/src/utils/debug.js
@@ -1,18 +1,17 @@
 import { compose } from 'redux'
+import globalContext from './global'
 
 export function Debug() {
   const config = arguments[0] || {} // eslint-disable-line
   // initialize global history
-  if (process.browser) window.__ANALYTICS_HISTORY__ = []
+  globalContext.__ANALYTICS_HISTORY__ = []
   return (createStore) => {
     return (reducer, preloadedState, enhancer) => {
       const store = createStore(reducer, preloadedState, enhancer)
       const origDispatch = store.dispatch
       const dispatch = (action) => {
         const a = action.action || action
-        if (process.browser) {
-          window.__ANALYTICS_HISTORY__.push(a)
-        }
+        globalContext.__ANALYTICS_HISTORY__.push(a)
         return origDispatch(action)
       }
       return Object.assign(store, { dispatch: dispatch })
