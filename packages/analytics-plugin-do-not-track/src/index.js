9f559e2f9768217218b8e9e04819c21a543f9d44
DavidWells <hello@davidwells.io>
2019-05-02 23:56:18 -0700

    refactor(do-not-track): add enable option. todo define API for fine grained permissions

--- packages/analytics-plugin-do-not-track/src/index.js
+++ packages/analytics-plugin-do-not-track/src/index.js
@@ -1,23 +1,27 @@
-const doNot = false
 
+// Todo design API for doNotTrack to allow for fine grained permissions
 export default function doNotTrackPlugin(userConfig = {}) {
   return {
     NAMESPACE: 'do-not-track',
     config: Object.assign({}, userConfig),
+    initializeStart: ({ abort, config }) => {
+      if (config.enabled) {
+        return abort('Cancel the initialize call because do-not-track plugin enabled')
+      }
+    },
     pageStart: ({ abort, config }) => {
-      if (doNot) {
-        // return abort('abort segment track call', ['segment'])
+      if (config.enabled) {
         return abort('Cancel the page call because do-not-track plugin enabled')
       }
     },
     identifyStart: ({ abort, config }) => {
-      if (doNot) {
+      if (config.enabled) {
         // return abort('abort segment track call', ['segment'])
         return abort('Cancel the identify call because do-not-track plugin enabled')
       }
     },
     trackStart: ({ abort, config }) => {
-      if (doNot) {
+      if (config.enabled) {
         // return abort('abort segment track call', ['segment'])
         return abort('Cancel the track call because do-not-track plugin enabled')
       }
