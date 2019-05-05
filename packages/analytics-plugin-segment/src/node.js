57ca6f3b04340dd820ad7ea08794cef0949ffe22
DavidWells <hello@davidwells.io>
2019-05-04 23:19:10 -0700

    refactor(segment-plugin): add disableAnonymousTraffic and flushInterval to segment node

--- packages/analytics-plugin-segment/src/node.js
+++ packages/analytics-plugin-segment/src/node.js
@@ -1,6 +1,6 @@
 /**
  * Segment.com Node plugin
- * https://github.com/segmentio/analytics-node
+ * uses https://github.com/segmentio/analytics-node
  */
 
 let Analytics
@@ -8,40 +8,62 @@ if (!process.browser) {
   Analytics = require('analytics-node')
 }
 
-const config = {}
+const config = {
+  /* Your segment write key */
+  writeKey: null,
+  /* Segment sdk flushInterval. Docs https://bit.ly/2H2jJMb */
+  flushInterval: 3,
+  /* Disable anonymous MTU */
+  disableAnonymousTraffic: false
+}
 
 const NAMESPACE = 'segment'
 
-let client
-
 /* Export the integration */
-export default function googleTagManager(userConfig) {
-  // Allow for userland overides of base methods
+export default function segment(userConfig = {}) {
+  // Initialize segment client
+  const segmentConfig = {
+    ...config,
+    ...userConfig
+  }
+  const client = new Analytics(segmentConfig.writeKey, {
+    flushInterval: segmentConfig.flushInterval
+  })
   return {
     NAMESPACE: NAMESPACE,
-    config: {
-      ...config,
-      ...userConfig
-    },
-    initialize: ({ config }) => {
-      client = new Analytics(config.writeKey)
-    },
-    // page view
+    config: segmentConfig,
+    /* page view */
     page: ({ payload, config }) => {
       client.page({
         properties: payload.properties
       })
     },
-    // track event
+    /* track event */
     track: ({ payload, config }) => {
-      // @Todo include userId
-      client.track({
-        // userId: '019mr8mf4r',
+      const { userId, anonymousId } = payload
+      if (!userId && !anonymousId) {
+        throw new Error('Missing userId and anonymousId. You must include one to make segment call')
+      }
+
+      const data = {
         event: payload.event,
         properties: payload.properties
-      })
+      }
+
+      if (userId) {
+        data.userId = userId
+      } else {
+        data.anonymousId = anonymousId
+      }
+
+      // Save boat loads of cash
+      if (config.disableAnonymousTraffic && !userId) {
+        return false
+      }
+
+      client.track(data)
     },
-    // identify user
+    /* identify user */
     identify: ({ payload }) => {
       const { userId, traits } = payload
       client.identify({ userId, traits })
