a2bdcc82dc43cf6bb91b5e13184d2db0a88c03bf
DavidWells <hello@davidwells.io>
2019-05-07 23:41:10 -0700

    docs(core): add plugin type defs for future use

---
+++ packages/analytics-core/src/pluginTypeDef.js
@@ -0,0 +1,12 @@
+/**
+  * @typedef {Object} AnalyticsPlugin
+  * @property {string} NAMESPACE - Name of plugin
+  * @property {Object} [EVENTS] - exposed events of plugin
+  * @property {Object} [config] - Configuration of plugin
+  * @property {function} [initialize] - Load analytics scripts method
+  * @property {function} [page] - Page visit tracking method
+  * @property {function} [track] - Custom event tracking method
+  * @property {function} [identify] - User identify method
+  * @property {function} [loaded] - Function to determine if analytics script loaded
+  * @property {function} [ready] - Fire function when plugin ready
+  */
