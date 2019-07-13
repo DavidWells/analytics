bf8701b6ea5ff39ed47b87251c569aeaa5da4f82
DavidWells <hello@davidwells.io>
2019-07-12 22:27:15 -0700

    feat: add onRouteChange utility

---
+++ packages/analytics-utils/src/onRouteChange.js
@@ -0,0 +1,21 @@
+import inBrowser from './inBrowser'
+import noOp from './noOp'
+
+export default function onRouteChange(callback = noOp) {
+  if (!inBrowser) return
+  const { addEventListener, history, location } = window
+  // Observe native navigation
+  addEventListener('popstate', () => {
+    callback(location.pathname)
+  })
+  // Observe manual navigation
+  const methods = ['push', 'replace']
+  methods.map(type => { // eslint-disable-line
+    const state = `${type}State`
+    const historyState = history[state]
+    history[state] = function() {
+      callback(arguments[2])
+      return historyState.apply(history, arguments)
+    }
+  })
+}
