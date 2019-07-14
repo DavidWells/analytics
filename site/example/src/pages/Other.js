cd18a9b7369c69a8613618cd6d42a759d2c3c3e1
DavidWells <hello@davidwells.io>
2019-07-14 12:58:32 -0700

    docs: add 404 page

--- site/example/src/pages/Other.js
+++ site/example/src/pages/Other.js
@@ -1,3 +1,5 @@
-import React from "react"
+import React from 'react'
 
-export default () => <div>Homepage in a user's site</div>
+export default function OtherPage() {
+  return <div>Other page</div>
+}
