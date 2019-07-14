89014218b4efa965935bf718b149082f61ddf79c
DavidWells <hello@davidwells.io>
2019-07-14 16:01:34 -0700

    docs: shadow search box until algolia setup

---
+++ site/example/src/gatsby-theme-oss-docs/components/search.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// Overrides site/gatsby-theme-oss-docs/src/components/search.js
+const Search = (props) => {
+  // no search yet
+  return null
+}
+
+export default Search
