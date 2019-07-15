8d228d0b80c8411373e3e7a5ff761d198dcb84ee
DavidWells <hello@davidwells.io>
2019-07-14 21:19:46 -0700

    docs: main nav links easier to click

--- site/gatsby-theme-base/src/components/sidebar-nav/index.js
+++ site/gatsby-theme-base/src/components/sidebar-nav/index.js
@@ -1,36 +1,38 @@
-import Category from './category';
-import PropTypes from 'prop-types';
-import React, {Component, Fragment} from 'react';
-import store from 'store';
-import styled from '@emotion/styled';
-import {Link, withPrefix} from 'gatsby';
-import {MdUnfoldLess, MdUnfoldMore} from 'react-icons/md';
-import {colors} from '../../utils/colors';
-import {smallCaps} from '../../utils/typography';
+import Category from './category'
+import PropTypes from 'prop-types'
+import React, {Component, Fragment} from 'react'
+import store from 'store'
+import styled from '@emotion/styled'
+import {Link, withPrefix} from 'gatsby'
+import {MdUnfoldLess, MdUnfoldMore} from 'react-icons/md'
+import {colors} from '../../utils/colors'
+import {smallCaps} from '../../utils/typography'
 
 const StyledList = styled.ul({
   marginLeft: 0,
   listStyle: 'none'
-});
+})
 
 const listItemStyles = {
   color: 'inherit',
   ':hover': {
     opacity: colors.hoverOpacity
   }
-};
+}
 
 const StyledListItem = styled.li({
   fontSize: '1rem',
   a: {
     ...listItemStyles,
     textDecoration: 'none',
+    display: 'block',
+    width: '80%',
     '&.active': {
       color: colors.primary,
       pointerEvents: 'none'
     }
   }
-});
+})
 
 const ExpandAll = styled.button(listItemStyles, smallCaps, {
   display: 'flex',
@@ -47,36 +49,36 @@ const ExpandAll = styled.button(listItemStyles, smallCaps, {
     marginLeft: -4,
     marginRight: 4
   }
-});
+})
 
-const SIDEBAR_NAV_STATE_KEY = 'sidebarNav';
+const SIDEBAR_NAV_STATE_KEY = 'sidebarNav'
 
 function getSidebarState() {
-  return store.get(SIDEBAR_NAV_STATE_KEY) || {};
+  return store.get(SIDEBAR_NAV_STATE_KEY) || {}
 }
 
 function setSidebarState(state) {
-  store.set(SIDEBAR_NAV_STATE_KEY, state);
+  store.set(SIDEBAR_NAV_STATE_KEY, state)
 }
 
 function getId(title) {
-  return withPrefix(title);
+  return withPrefix(title)
 }
 
 export default class SidebarNav extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
-    const sidebarState = getSidebarState();
-    const activeCategory = props.contents.find(this.isCategorySelected);
+    const sidebarState = getSidebarState()
+    const activeCategory = props.contents.find(this.isCategorySelected)
     if (activeCategory) {
-      sidebarState[getId(activeCategory.title)] = true;
-      setSidebarState(sidebarState);
+      sidebarState[getId(activeCategory.title)] = true
+      setSidebarState(sidebarState)
     }
 
     this.state = {
       sidebarState
-    };
+    }
   }
 
   static propTypes = {
@@ -90,15 +92,15 @@ export default class SidebarNav extends Component {
   get allExpanded() {
     return this.props.contents.every(
       ({title}) => this.state.sidebarState[getId(title)]
-    );
+    )
   }
 
   isPageSelected = ({path}) => {
     const [prefixedPath, pathname] = [
       withPrefix(path),
       this.props.pathname
-    ].map(string => string.replace(/\/$/, ''));
-    return prefixedPath === pathname;
+    ].map(string => string.replace(/\/$/, ''))
+    return prefixedPath === pathname
   };
 
   isCategorySelected = ({path, pages}) =>
@@ -106,36 +108,36 @@ export default class SidebarNav extends Component {
 
   toggleCategory = title => {
     this.setState(prevState => {
-      const id = getId(title);
-      const expanded = !prevState.sidebarState[id];
+      const id = getId(title)
+      const expanded = !prevState.sidebarState[id]
       const sidebarState = {
         ...getSidebarState(),
         [id]: expanded
-      };
-
-      if (this.props.onToggleCategory) {
-        this.props.onToggleCategory(title, expanded);
       }
 
-      setSidebarState(sidebarState);
-      return {sidebarState};
-    });
+      if (this.props.onToggleCategory) {
+        this.props.onToggleCategory(title, expanded)
+      }
+
+      setSidebarState(sidebarState)
+      return {sidebarState}
+    })
   };
 
   toggleAll = () => {
-    const expanded = !this.allExpanded;
+    const expanded = !this.allExpanded
     const sidebarState = this.props.contents.reduce(
       (acc, {title}) => ({
         ...acc,
         [getId(title)]: expanded
       }),
       {}
-    );
+    )
 
-    setSidebarState(sidebarState);
-    this.setState({sidebarState});
+    setSidebarState(sidebarState)
+    this.setState({sidebarState})
     if (this.props.onToggleAll) {
-      this.props.onToggleAll(expanded);
+      this.props.onToggleAll(expanded)
     }
   };
 
@@ -145,7 +147,9 @@ export default class SidebarNav extends Component {
         {pages.map(page => (
           <StyledListItem key={page.path}>
             {page.anchor ? (
-              <a href={page.path}>{page.title}</a>
+              <a href={page.path}>
+                {page.title}
+              </a>
             ) : (
               <Link
                 className={this.isPageSelected(page) ? 'active' : null}
@@ -157,16 +161,16 @@ export default class SidebarNav extends Component {
           </StyledListItem>
         ))}
       </StyledList>
-    );
+    )
   }
 
   render() {
     return (
       <Fragment>
         {this.props.contents.map(({title, path, pages}, index, array) => {
-          const contents = this.renderPages(pages);
+          const contents = this.renderPages(pages)
           if (!title) {
-            const Icon = this.allExpanded ? MdUnfoldLess : MdUnfoldMore;
+            const Icon = this.allExpanded ? MdUnfoldLess : MdUnfoldMore
             return (
               <Fragment key="root">
                 {contents}
@@ -177,7 +181,7 @@ export default class SidebarNav extends Component {
                   </ExpandAll>
                 )}
               </Fragment>
-            );
+            )
           }
 
           return (
@@ -195,9 +199,9 @@ export default class SidebarNav extends Component {
             >
               {contents}
             </Category>
-          );
+          )
         })}
       </Fragment>
-    );
+    )
   }
 }
