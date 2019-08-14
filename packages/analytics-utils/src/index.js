9554ef95614cb4444bae2db2a91b57ed08baf1c4
DavidWells <hello@davidwells.io>
2019-08-13 21:15:26 -0700

    refactor: remove unused utils

--- packages/analytics-utils/src/index.js
+++ packages/analytics-utils/src/index.js
@@ -1,13 +1,11 @@
 
 export { default as cookie } from './cookie.js'
 export { default as decodeUri } from './decodeUri.js'
-export { default as detectAdBlock } from './detectAdBlock.js'
 export { default as getBrowserLocale } from './getBrowserLocale.js'
 export { default as getTimeZone } from './getTimeZone.js'
 export { default as inBrowser } from './inBrowser.js'
 export { default as isExternalReferrer } from './isExternalReferrer.js'
 export { default as isScriptLoaded } from './isScriptLoaded.js'
-export { default as onRouteChange } from './onRouteChange.js'
 export { default as noOp } from './noOp.js'
 export { default as paramsClean } from './paramsClean.js'
 export { default as paramsGet } from './paramsGet.js'
