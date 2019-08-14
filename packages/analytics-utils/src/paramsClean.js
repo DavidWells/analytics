b0a12be962bf9b4fd8f9ec3b6aaf2c261347f275
DavidWells <hello@davidwells.io>
2019-08-13 21:21:45 -0700

    refactor: update clean param regex

--- packages/analytics-utils/src/paramsClean.js
+++ packages/analytics-utils/src/paramsClean.js
@@ -6,12 +6,12 @@
  * @return {string} cleaned url
  */
 export default function paramsClean(url, param) {
-  const search = (url.split('?') || [ , ])[1]
+  const search = (url.split('?') || [ , ])[1] // eslint-disable-line
   if (!search || search.indexOf(param) === -1) {
     return url
   }
   // remove all utm params from URL search
-  const regex = new RegExp(`(\\&|\\?)${param}([_A-Za-z0-9"+=\.%]+)`, 'g')
+  const regex = new RegExp(`(\\&|\\?)${param}([_A-Za-z0-9"+=.\\/\\-@%]+)`, 'g')
   const cleanSearch = `?${search}`.replace(regex, '').replace(/^&/, '?')
   // replace search params with clean params
   const cleanURL = url.replace(`?${search}`, cleanSearch)
