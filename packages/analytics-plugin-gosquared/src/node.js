50460b7861b3299962dbd4758138a3f6f79b59d5
Ben Clark <benclark@Bens-MacBook-Pro.local>
2019-12-04 09:47:03 +0000

    Add GoSquared plugin

---
+++ packages/analytics-plugin-gosquared/src/node.js
@@ -0,0 +1,10 @@
+
+export default function goSquared(pluginConfig) {
+  return {
+    NAMESPACE: 'gosquared',
+    initialize: ({ config }) => {
+      console.log('GoSquared has no server implementation')
+    }
+  }
+}
+``
