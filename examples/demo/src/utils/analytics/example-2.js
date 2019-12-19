470fffc206e1267af2e57bea90e49891a3ff3bc5
DavidWells <hello@davidwells.io>
2019-12-18 20:10:19 -0800

    docs: add methods to demo plugin

--- examples/demo/src/utils/analytics/example-2.js
+++ examples/demo/src/utils/analytics/example-2.js
@@ -11,15 +11,53 @@ const analytics = Analytics({
   plugins: [
     {
       name: 'hahahaa',
+      methods: {
+        thingNo(one, two, three) {
+          console.log('thing one')
+        },
+        xyz(one, two, three) {
+          console.log('thing xyz')
+        },
+      },
       track: () => {
         console.log('hi')
-      }
+      },
+      rad: () => {
+        console.log('RADDDD')
+      },
     },
     {
       NAMESPACE: 'hello',
       track: () => {
         console.log('hi')
-      }
+      },
+      methods: {
+        thing(one, two, three) {
+          const additionalArg = arguments[arguments.length - 1]
+          const { instance } = this
+          console.log('this', this)
+          console.log('additionalArg', additionalArg)
+          console.log('one', one)
+          console.log('two', two)
+          console.log('three', three)
+          console.log('instance', this.instance)
+          console.log('state', additionalArg.getState())
+          this.instance.dispatch('radStart')
+        },
+        otherThing: (one, ...args) => {
+          const additionalArg = args[args.length - 1]
+          console.log('additionalArg', additionalArg)
+          console.log('otherThing', one)
+          additionalArg.on('radStart', () => {
+            console.log('Do the silly thing')
+          })
+        },
+        hey: (one, ...args) => {
+          const instance = args[args.length - 1]
+          console.log('instance', instance)
+          console.log('this', this)
+        },
+      },
     },
     exampleProviderPlugin({
       settingOne: 'xyz'
