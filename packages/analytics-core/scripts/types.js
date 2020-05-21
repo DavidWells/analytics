4faa1870b1dd52a992ed38b37336c609e3202618
DavidWells <hello@davidwells.io>
2020-05-21 08:07:22 -0700

    chore: update types generation
    Signed-off-by: DavidWells <hello@davidwells.io>

--- packages/analytics-core/scripts/types.js
+++ packages/analytics-core/scripts/types.js
@@ -1,22 +1,31 @@
+// https://github.com/englercj/tsd-jsdoc/issues/64#issuecomment-462020832
 const fs = require('fs')
 const path = require('path')
-// https://github.com/englercj/tsd-jsdoc/issues/64#issuecomment-462020832
+const indentString = require('indent-string')
 
 const TYPES_PATH = path.resolve(__dirname, '../temp-types/types.d.ts')
-// analytics.cjs.d.ts
 const OUTPUT_PATH = path.resolve(__dirname, '../lib/types.d.ts')
 const content = fs.readFileSync(TYPES_PATH, 'utf-8')
 
-// Export not needed
-const exportDeclaration = content.replace(/^(declare function analytics)/gm, 'export function analytics')
+const typesFromJsDocs = content
+  // Remove declares
+  .replace(/^declare\s/gm, '')
+  // Make promises return void
+  .replace(/\@returns \{Promise\}/gm, '@returns {Promise<void>}')
+  // Fix plugin interface
+  .replace(/plugins\?: object\[\]/gm, 'plugins?: Array<AnalyticsPlugin>')
 
 // Expose main API
-const newContent = `${content}
-export const CONSTANTS: constants;
+const newContent = `declare module "analytics" {
+${indentString(typesFromJsDocs, 2)}
+  export const CONSTANTS: constants;
 
-export const init = analytics;
+  export const init: typeof analytics;
 
-export const Analytics = analytics;
+  export const Analytics: typeof analytics;
 
-export = analytics;`
+  export = analytics;
+}`
+
+// Write out to types.d.ts file in /lib
 fs.writeFileSync(OUTPUT_PATH, newContent)
