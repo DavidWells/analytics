f458a1d32556c80f96dbcf9abbfa060b77ac4da0
DavidWells <hello@davidwells.io>
2020-05-21 22:23:09 -0700

    docs: update perfume demo
    Signed-off-by: DavidWells <hello@davidwells.io>

--- examples/using-perfumejs/src/App.js
+++ examples/using-perfumejs/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Analytics from 'analytics'
+import Perfume from 'perfume.js'
 import googleAnalytics from '@analytics/google-analytics'
-import segmentPlugin from '@analytics/segment'
+import perfumePlugin from '@analytics/perfumejs'
 import customAnalyticsPlugin from './plugins/custom'
-import perfumePlugin from './plugins/perfume'
 import './App.css'
 
 /**
@@ -11,47 +11,46 @@ import './App.css'
  * third party analytics tool you'd like the perf data to be sent to
  */
 const analytics = Analytics({
-  app: 'my-app-name',
+  app: 'my-app',
   plugins: [
-    perfumePlugin({
-      category: 'perfMetrics'
-    }),
+    {
+      name: 'test-plugin',
+      track: ({ payload }) => {
+        console.log('payload', payload)
+        // Send perf information to your own backend API here 
+      }
+    },
     // Custom plugins to now send perf data to
     customAnalyticsPlugin,
-    // Send perfume.js data to GA
     googleAnalytics({
-      trackingId: 'UA-126647663-7' // <--- your GA ID
+      trackingId: 'UA-126647663-7'
     }),
-    // Send perfume.js data to segment
-    // segmentPlugin({
-    //   writeKey: '123-xyz'  // <--- your segment key
-    // })
-    // ... etc. Any analytics plugin with `track` call will work https://getanalytics.io/plugins/
+    perfumePlugin({
+      category: 'perf',
+      perfume: Perfume
+    }),
   ]
 })
 
 export default function App() {
-  /* Other analytics method are now also available for use
-  // Track page views
-  analytics.page()
-
-  // Identify users
-  analytics.identify('userid-123', {
-    favoriteColor: 'blue',
-    membershipLevel: 'pro'
-  })
-
-  // Track events
-  analytics.track('buttonClicked', {
-    value: 100
-  })
-  */
   return (
     <div className="App">
       <h1>Using perfume.js with analytics</h1>
       <p>Perfume is a tiny, web performance monitoring library that reports field data back to your favorite analytics tool.</p>
       <p>Open the console to see perf metrics sending sent</p>
       <a href="https://zizzamia.github.io/perfume/">Read more about perfume.js</a>
+      <br/><br/><br/>
+      <div>
+        <button onClick={() => analytics.track('buttonClicked', { color: 'blue' })}>
+          Track
+        </button>
+        <button onClick={() => analytics.identify('user-xyz')}>
+          Identify
+        </button>
+        <button onClick={() => analytics.page()}>
+          Page View
+        </button>
+      </div>
     </div>
   )
 }
