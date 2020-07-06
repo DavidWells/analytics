7fa04581eaa48b8d9368d28fedc6e2638207c8a2
DavidWells <hello@davidwells.io>
2020-07-05 23:15:27 -0700

    refactor: export globalContext from storage
    Signed-off-by: DavidWells <hello@davidwells.io>

--- packages/analytics-util-storage/src/utils/globalContext.js
+++ packages/analytics-util-storage/src/utils/globalContext.js
@@ -1,18 +1,7 @@
-// import undef from './undef'
-/* global self globalThis */
 
-export default function getGlobalThis() {
-  if (typeof globalThis !== 'undefined') return globalThis
-  if (typeof global !== 'undefined') return global
-  if (typeof self !== 'undefined') return self /* eslint-disable-line no-restricted-globals */
-  if (typeof window !== 'undefined') return window
-  if (typeof this !== 'undefined') return this
-  return {} // should never happen
-}
-
-/* tinier from https://github.com/purposeindustries/window-or-global/blob/master/lib/index.js
-const context = (typeof self === 'object' && self.self === self && self) || // eslint-disable-line
+const globalContext =
+  (typeof self === 'object' && self.self === self && self) ||
   (typeof global === 'object' && global.global === global && global) ||
   this
-export default context
-*/
+
+export default globalContext
