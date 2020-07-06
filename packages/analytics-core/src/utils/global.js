468d95100de7995fa2c950a84a4c03658175f88a
DavidWells <hello@davidwells.io>
2020-07-05 23:15:38 -0700

    refactor: remove globalContext
    Signed-off-by: DavidWells <hello@davidwells.io>

--- packages/analytics-core/src/utils/global.js
+++
@@ -1,7 +0,0 @@
-/* from https://github.com/purposeindustries/window-or-global/blob/master/lib/index.js */
-
-const context = (typeof self === 'object' && self.self === self && self) || // eslint-disable-line
-  (typeof global === 'object' && global.global === global && global) ||
-  this
-
-export default context
