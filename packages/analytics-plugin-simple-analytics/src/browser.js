ec8579b2a48767a99fa43e58ea633010cc69e065
Tim Simon <tim@mirahi.io>
2020-09-29 11:58:04 +0200

    fix: 🐛 use latest simple analytics script

--- packages/analytics-plugin-simple-analytics/src/browser.js
+++ packages/analytics-plugin-simple-analytics/src/browser.js
@@ -17,8 +17,8 @@ export default function simpleAnalyticsPlugin(pluginConfig = {}) {
     config: pluginConfig,
     // https://docs.simpleanalytics.com/script
     initialize: ({ config }) => {
-      const domain = config.customDomain || 'https://cdn.simpleanalytics.io'
-      const src = `${domain}/hello.js`
+      const domain = config.customDomain || 'scripts.simpleanalyticscdn.com'
+      const src = `https://${domain}/latest.js`
       const script = document.createElement('script')
       script.type = 'text/javascript'
       script.async = true
