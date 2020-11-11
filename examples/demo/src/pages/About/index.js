11fedae130602a7121f04f2e396ef0b085139d8f
DavidWells <hello@davidwells.io>
2020-11-11 10:22:41 -0800

    docs: update about page
    Signed-off-by: DavidWells <hello@davidwells.io>

--- examples/demo/src/pages/About/index.js
+++ examples/demo/src/pages/About/index.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Navigation from '../../fragments/Nav'
+import analytics from '../../utils/analytics'
 
-const About = () => (
-  <div>
-    <Navigation />
-    <h1>About</h1>
+
+const About = () => {
+  useEffect(() => {
+    const state = analytics.getState('page')
+    console.log('page state', state)
+    analytics.track('foobar', () => {
+      console.log('tracked foobar')
+    })
+  }, [])
+  return (
     <div>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae mauris arcu, eu pretium nisi.
-      Praesent fringilla ornare ullamcorper. Pellentesque diam orci, sodales in blandit ut, placerat quis felis.
-      Vestibulum at sem massa, in tempus nisi. Vivamus ut fermentum odio. Etiam porttitor faucibus volutpat.
-      Vivamus vitae mi ligula, non hendrerit urna. Suspendisse potenti. Quisque eget massa a massa semper mollis.
+      <Navigation />
+      <h1>About</h1>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer vitae mauris arcu, eu pretium nisi.
+        Praesent fringilla ornare ullamcorper. Pellentesque diam orci, sodales in blandit ut, placerat quis felis.
+        Vestibulum at sem massa, in tempus nisi. Vivamus ut fermentum odio. Etiam porttitor faucibus volutpat.
+        Vivamus vitae mi ligula, non hendrerit urna. Suspendisse potenti. Quisque eget massa a massa semper mollis.
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default About
