a832d778cb7ce24d2e38ad026e190df0a6c4e84c
DavidWells <>
2020-12-22 20:46:54 -0800

    docs: update basic example
    Signed-off-by: DavidWells <>

--- examples/demo/src/utils/analytics/basic.js
+++ examples/demo/src/utils/analytics/basic.js
@@ -1,4 +1,9 @@
 import Analytics from 'analytics'
+import googleAnalytics from '@analytics/google-analytics'
+
+const customGA = googleAnalytics({
+  trackingId: process.env.REACT_APP_GOOGLE_ANALYTICS_ID,
+})
 
 /* initialize analytics and load plugins */
 const analytics = Analytics({
@@ -6,12 +11,35 @@ const analytics = Analytics({
   app: 'yolo',
   plugins: [
     {
+      ...customGA,
+      enabled: false,
+    },
+    {
+      name: 'plugin-x',
+      enabled: false,
+      initialize: () => {
+        console.log('FIRE INIT FROM PLUGIN X')
+      },
+      page: ({ payload }) => console.log('plugin-x page view', payload),
+    },
+    {
+      name: 'plugin-y',
+      enabled: false,
+      page: ({ payload }) => console.log('plugin-y page view', payload),
+    },
+    {
       name: 'plugin-1',
-      page: ({ payload }) => console.log('page view 1', payload)
+      initialize: () => {
+        console.log('1')
+      },
+      page: ({ payload }) => console.log('plugin-1 page view', payload),
     },
     {
       name: 'plugin-2',
-      page: ({ payload }) => console.log('page view 2', payload)
+      initialize: () => {
+        console.log('2')
+      },
+      page: ({ payload }) => console.log('plugin-2 page view', payload)
     },
   ]
 })
