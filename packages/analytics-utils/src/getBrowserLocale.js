09b1632524011b2518386d862164f18f3ca3ed9f
DavidWells <>
2021-01-03 16:01:46 -0800

    refactor: simplify getBrowserLocale
    Signed-off-by: DavidWells <>

--- packages/analytics-utils/src/getBrowserLocale.js
+++ packages/analytics-utils/src/getBrowserLocale.js
@@ -1,16 +1,8 @@
 import inBrowser from './inBrowser'
 
 export default function getBrowserLocale() {
-  if (!inBrowser) return null
+  if (!inBrowser) return
   const { language, languages, userLanguage } = navigator
-  if (languages && languages.length) {
-    // latest versions of Chrome and Firefox set this correctly
-    return languages[0]
-  }
-  // IE only
-  if (userLanguage) {
-    return userLanguage
-  }
-  // latest versions of Chrome, Firefox, and Safari set this correctly
-  return language
+  if (userLanguage) return userLanguage // IE only
+  return (languages && languages.length) ? languages[0] : language
 }
