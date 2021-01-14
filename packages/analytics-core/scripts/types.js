69c3fbe19cf88dd52ff50fd400b7c1ac71425c3c
DavidWells <>
2021-01-13 22:25:08 -0800

    refactor: make types extendable
    Signed-off-by: DavidWells <>

--- packages/analytics-core/scripts/types.js
+++ packages/analytics-core/scripts/types.js
@@ -8,20 +8,61 @@ const TYPES_PATH = path.resolve(__dirname, '../temp-types/types.d.ts')
 const OUTPUT_PATH = path.resolve(__dirname, '../lib/types.d.ts')
 const content = fs.readFileSync(TYPES_PATH, 'utf-8')
 
+// const PAGE_REGEX = /type PageData = {(?:.*?|\n)*?}/gm
+// const PLUGIN_REGEX = /type AnalyticsPlugin = {(?:.*?|\n)*?}/gm
+
 const typesFromJsDocs = content
   // Remove declares
   .replace(/^declare\s/gm, '')
-  // Export analytics instance
-  .replace(/type AnalyticsInstance =/gm, 'export type AnalyticsInstance =')
+  // Export user-facing types (and where possible, convert objects to interface so they can be extended) 
+  // via https://github.com/DavidWells/analytics/blob/2712604ff90ffd0baf4ab62c7f12267b07b0d823/packages/analytics-core/scripts/types.js#L15
+  .replace(/type AnalyticsInstance =/gm, 'export interface AnalyticsInstance ')
+  .replace(/type AnalyticsInstanceConfig =/gm, 'export interface AnalyticsInstanceConfig ')
+  .replace(/type (\w+)Payload =/gm, match => `export ${match}`)
+  // Exported so user doesn't have to do ReturnType to get them
+  .replace(/type DetachListeners =/gm, 'export type DetachListeners =')
+
+  // Convert following types to generics to be able to pass payload type throught them
+  // Export hooks and convert them to generics accepting payload type
+  .replace(
+    /type (\w+)Hook =(.*)Context/gm,
+    (_, typeName, argsDefStart) => `export type ${typeName}Hook<T = ${typeName}Payload> =${argsDefStart}Context<T>`,
+  )
+  // Convert contexts to generics accepting payload type
+  .replace(
+    /type (\w+)Context =(.*)ContextProps(\W)/gm,
+    (_, typeName, typeDefStart, typeDefEnd) => `type ${typeName}Context<T = ${typeName}Payload> =${typeDefStart}ContextProps<T>${typeDefEnd}`,
+  )
+  // Convert context props types to generics accepting payload type
+  .replace(
+    /type (\w+)ContextProps =((.|\s)*?)payload: \w+/gm,
+    (_, typeName, typeDefStart) => `type ${typeName}ContextProps<T = ${typeName}Payload> =${typeDefStart}payload: T`,
+  )
+
+  // Rename following types so we can set generics in their place
+  .replace(/type AnalyticsPlugin = /gm, 'interface AnalyticsPluginBase ')
+  .replace(/type PageData = /gm, 'interface PageDataBase ')
   // Make promises return void
   .replace(/\@returns \{Promise\}/gm, '@returns {Promise<void>}')
   .replace(/=> Promise;/gm, '=> Promise<any>;')
-  // Fix plugin interface
-  .replace(/plugins\?: object\[\]/gm, 'plugins?: Array<AnalyticsPlugin>')
+  // Convert unions ('|') to joins ('&').
+  // Joins are used for modular JSDOC typedefs that support intellisense in VS Code.
+  // 'jsdoc' cannot parse joins, so they are temporarily transpiled to unions by 'jsdoc-plugin-intersection'.
+  .replace(/ \| /gm, ' & ')
+
+  // Make types extensible
+const typeExtensions = `
+  export type PageData<T extends string = string> = PageDataBase & Record<T, unknown>;
+  export type AnalyticsPlugin<T extends string = string> = AnalyticsPluginBase & string extends T
+    ? Record<string, unknown>
+    : Record<T, Hook> & Record<string, unknown>;
+`;
 
 // Expose main API
 const newContent = `declare module "analytics" {
 ${indentString(typesFromJsDocs, 2)}
+${typeExtensions}
+
   export const CONSTANTS: constants;
 
   export const init: typeof analytics;
@@ -29,7 +70,7 @@ ${indentString(typesFromJsDocs, 2)}
   export const Analytics: typeof analytics;
 
   export default analytics;
-}`
+}`// .replace(/;;/g, ';')
 
 mkdirp(path.dirname(OUTPUT_PATH), function (err) {
   if (err) console.error(err)
