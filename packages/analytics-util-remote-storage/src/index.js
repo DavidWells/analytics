d92684800205b3af304f76d72f073fbbc031eab2
DavidWells <>
2021-02-07 23:00:55 -0800

    feat: add key to resolver
    Signed-off-by: DavidWells <>

--- packages/analytics-util-remote-storage/src/index.js
+++ packages/analytics-util-remote-storage/src/index.js
@@ -104,6 +104,7 @@ async function setRemoteItem(storageKey, storageValue, opts = {}, instance) {
   const localParsed = safeParse(value)
 
   const newValue = await resolve({
+    key: storageKey,
     local: localParsed,
     localRaw: value,
     remote: remoteParsed,
