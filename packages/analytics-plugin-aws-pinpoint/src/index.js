f3bd8f77461fd8460cabc53d9df1d3bb8b8d9a83
David Wells <>
2021-04-19 13:49:45 -0700

    refactor: export formatEvent utility
    Signed-off-by: David Wells <>

--- packages/analytics-plugin-aws-pinpoint/src/index.js
+++ packages/analytics-plugin-aws-pinpoint/src/index.js
@@ -1,8 +1,9 @@
 import serverSide from './node'
 import clientSide from './browser'
 import * as PINPOINT_EVENTS from './pinpoint/events'
+import { formatEvent } from './pinpoint'
 
 /* This module will shake out unused code + work in browser and node 🎉 */
 export default process.browser ? clientSide : serverSide
 
-export { PINPOINT_EVENTS }
+export { PINPOINT_EVENTS, formatEvent }
