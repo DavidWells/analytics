c3d5500c2d6d1867afc357663deced32f5a484a0
David Wells <>
2021-05-23 19:31:46 -0700

    feat: add @analytics/session-utils library
    Signed-off-by: David Wells <>

---
+++ packages/analytics-util-session/README.md
@@ -0,0 +1,57 @@
+<!--
+title: Javascript session utils
+pageTitle: Session utils
+description: Session library for user sessions
+-->
+
+# Session Utilities
+
+Tiny session utility library.
+
+- Persisted Sessions - saved as cookie for 30min
+  - `getSession`
+  - `setSession` 
+- Tab Sessions - saved until tab/window closed
+  - `getTabSession`
+  - `setTabSession`
+- Page Sessions - saved until page changes
+  - `getPageSession`
+  - `setPageSession`
+
+## Example
+
+```js
+import { getSession, setSession } from '@analytics/session-utils'
+
+const currentSession = getSession()
+console.log('currentSession', currentSession)
+
+// Update session
+const newSessionInfo = setSession()
+console.log('newSessionInfo', newSessionInfo)
+```
+
+## About
+
+```
+┌─────────────────────────────────────────────────────────────────┐
+│                        Persisted Session                        │
+│                                                                 │
+│   ┌──────────────────────────┐    ┌──────────────────────────┐  │
+│   │       Tab Session        │    │       Tab Session        │  │
+│   │                          │    │                          │  │
+│   │  ┌────────────────────┐  │    │  ┌────────────────────┐  │  │
+│   │  │    Page Session    │  │    │  │    Page Session    │  │  │
+│   │  │                    │  │    │  │                    │  │  │
+│   │  │                    │  │    │  │                    │  │  │
+│   │  │                    │  │    │  │                    │  │  │
+│   │  │                    │  │    │  │                    │  │  │
+│   │  │                    │  │    │  │                    │  │  │
+│   │  │                    │  │    │  │                    │  │  │
+│   │  │                    │  │    │  │                    │  │  │
+│   │  │                    │  │    │  │                    │  │  │
+│   │  └────────────────────┘  │    │  └────────────────────┘  │  │
+│   └──────────────────────────┘    └──────────────────────────┘  │
+│                                                                 │
+└─────────────────────────────────────────────────────────────────┘
+```
---
+++ packages/analytics-util-session/src/index.js
@@ -0,0 +1,96 @@
+// Session Utilities
+import { uuid, globalContext, inBrowser } from 'analytics-utils'
+import { setCookie, getCookie } from '@analytics/cookie-utils'
+
+const PREFIX = '__'
+const SESSION = 'session'
+const PAGE = 'page'
+const KEYS = ['id', 'date', 'epoch']
+
+function isSessionSupported() {
+  if (inBrowser) {
+    try {
+      sessionStorage.setItem(PREFIX, PREFIX)
+      sessionStorage.removeItem(PREFIX)
+      return true
+    } catch (e) {}
+  }
+  return false
+}
+
+const hasSessionStorage = isSessionSupported()
+
+function getSessionItem(key, defaultValue) {
+  let value = (hasSessionStorage) ? sessionStorage.getItem(key) : getGlobal(key)
+  if (value == null && defaultValue) {
+    value = setSessionItem(key, defaultValue)
+  }
+  return value
+}
+
+function setSessionItem(key, value) {
+	if (hasSessionStorage) {
+    sessionStorage.setItem(key, value)
+    return value
+	}
+  return setGlobal(key, value)
+}
+
+function setGlobal(key, val) {
+  globalContext[key] = val
+  return val
+}
+
+function getGlobal(key) {
+  return globalContext[key]
+}
+
+const storageMechanism = {
+  session: [ getSessionItem, setSessionItem ],
+  page: [ getGlobal, setGlobal ]
+}
+
+export function sessionDetails() {
+  const d = new Date()
+  return {
+    id: uuid(),
+    date: d.toISOString(),
+    epoch: d.getTime(),
+  }
+}
+
+function logic(kind, isSetter) {
+  const data = sessionDetails()
+  const [ get, set ] = storageMechanism[kind]
+  const info = Object.fromEntries(KEYS.map((key) => {
+    const k = PREFIX + kind + key
+    const currentVal = get(k)
+    const value = (currentVal && !isSetter) ? currentVal : set(k, data[key])
+    const finValue = (key !== 'epoch') ? value : Number(value)
+    return [ key, finValue ]
+  }))
+  return addElasped(info)
+}
+
+function addElasped(obj) {
+  obj.elapsed = Date.now() - obj.epoch
+  return obj
+}
+
+export function getSession()  {
+  const cookieData = getCookie(PREFIX + SESSION)
+  const data = (cookieData) ? JSON.parse(cookieData) : setPersistedSession()
+  return addElasped(data)
+}
+
+export function setSession()  {
+  const data = sessionDetails()
+  setCookie(PREFIX + SESSION, JSON.stringify(data), 60*30)
+  return data
+}
+
+export const getTabSession = logic.bind(null, SESSION)
+export const setTabSession = logic.bind(null, SESSION, true)
+
+export const getPageSession = logic.bind(null, PAGE)
+export const setPageSession = logic.bind(null, PAGE, true)
