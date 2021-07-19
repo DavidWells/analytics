d8099c8e060ca8901bf7150a7c3c0531d67ff961
David Wells <>
2021-07-19 10:37:06 -0700

    chore: version pinpoint
    Signed-off-by: David Wells <>

--- packages/analytics-plugin-aws-pinpoint/src/index.js
+++ packages/analytics-plugin-aws-pinpoint/src/index.js
@@ -5,4 +5,5 @@ import { formatEvent } from './pinpoint'
 
 /* This module will shake out unused code + work in browser and node 🎉 */
 export default process.browser ? clientSide : serverSide
+
 export { PINPOINT_EVENTS, formatEvent }
