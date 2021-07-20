bc81b9219e234b759c9537c98a17bf122b1508d7
David Wells <>
2021-07-19 22:50:09 -0700

    docs: add docblocks
    Signed-off-by: David Wells <>

--- packages/analytics-util-global-storage/src/index.js
+++ packages/analytics-util-global-storage/src/index.js
@@ -3,22 +3,37 @@ const globalContext =
   (typeof global === 'object' && global.global === global && global) ||
   this
 
+/**
+ * Get value from global context
+ * @param {string} key - Key of value to get
+ * @returns {*} value
+ */
 function get(key) {
   return globalContext[key]
 }
 
+/**
+ * Set value to global context
+ * @param {string} key - Key of value to set
+ * @param {*} value 
+ * @returns value
+ */
 function set(key, value) {
   globalContext[key] = value
   return value
 }
 
+/**
+ * Remove value to global context
+ * @param {string} key - Key of value to remove
+ */
 function remove(key) {
   set(key, undefined)
 }
 
 export {
-  globalContext,
   get,
   set,
-  remove
+  remove,
+  globalContext,
 }
