cd756a3772f06b054aa244efb168480817a8a4ed
David Wells <>
2021-07-26 18:47:30 -0700

    docs: add doc pointer
    Signed-off-by: David Wells <>

--- packages/analytics/src/index.js
+++ packages/analytics/src/index.js
@@ -1,6 +1,7 @@
 import analytics, { EVENTS, CONSTANTS } from '@analytics/core'
 import storage from '@analytics/storage-utils'
 
+// See ../packages/analytics-core for source code
 export default function analyticsLib(opts = {}) {
   const defaultSettings = { storage }
   return analytics({
