3a5cb9c8f087c7714f1b7de3e49633bddb1c6425
David Wells <>
2021-07-30 18:51:18 -0700

    refactor: shrink listener util
    Signed-off-by: David Wells <>

--- packages/analytics-util-listener/README.md
+++ packages/analytics-util-listener/README.md
@@ -6,7 +6,7 @@ description: Utility library for adding backwards compatible event listeners
 
 # Listener Utilities
 
-A tiny utility library for working with event listeners in <!-- AUTO-GENERATED-CONTENT:START (pkgSize) -->`649 bytes`<!-- AUTO-GENERATED-CONTENT:END -->.
+A tiny utility library for working with event listeners in <!-- AUTO-GENERATED-CONTENT:START (pkgSize) -->`563 bytes`<!-- AUTO-GENERATED-CONTENT:END -->.
 
 Exposes `addListener`, `removeListener` functions.
 
--- packages/analytics-util-listener/src/index.js
+++ packages/analytics-util-listener/src/index.js
@@ -1,90 +1,57 @@
+import { isBrowser, isArray, isString, noOp } from '@analytics/type-utils'
+
 const EVENT = 'Event'
-const ADD = ['add', 'attach']
-const REMOVE = ['remove', 'detach']
-const ACTIONS = [ ADD, REMOVE ]
 const EventListener = EVENT + 'Listener'
-const noOp = () => {}
 
 function createListener(add) {
-  const [ action, inverse ] = (add) ? ACTIONS : ACTIONS.reverse()
-  const method = action[0] + EventListener
-  return (elements, evts, callback, opts) => {
+  return (els, evts, callback, opts) => {
     const handler = callback || noOp
     /* SSR support */
-    if (typeof window === 'undefined') {
-      return handler
-    }
-    var options = opts || false
-    var events = toArray(evts)
-    var els = toArray(isString(elements) ? document.querySelectorAll(elements) : elements)
-    var connectListener
+    if (!isBrowser) return handler
+    
+    const options = opts || false
+    const events = toArray(evts)
+    const elements = toArray(els, true)
+    let fns = []
     /* Throw if no element found */
-    if (!els.length) throw new Error('noElements')
+    if (!elements.length) throw new Error('noElements')
     /* Throw if no events found */
     if (!events.length) throw new Error('no' + EVENT)
-    /* use add/removeEventListener */
-    if (els[0][method]) {
-      connectListener = fireAndReturn(() => {
-        return els.map((el) => {
-          const func = oncify(handler, options)
-          events.forEach((evt) => el[method](evt, func, options))
-          return func
-        })
-      })
-      /* Return cleanup with re-initialize function */
-      return function() {
-        els.forEach((el, i) => events.forEach((ev) => {
-          el[inverse[0] + EventListener](ev, connectListener.listeners[i], options)
-        }))
-        return connectListener.fn
+  
+    function smartAttach(isAdd) {
+      const method = (isAdd) ? 'add' + EventListener : 'remove' + EventListener
+      // console.log((isAdd) ? '>> setup called' : '>> teardown')
+      // console.log(`>>> with ${method}`)
+      if (isAdd) fns = []
+      
+      // Apply to all elements
+      for (let i = 0; i < elements.length; i++) {
+        const el = elements[i]
+        fns[i] = (isAdd ? oncify(handler, options) : fns[i] || handler)
+        // Apply to all events
+        for (let n = 0; n < events.length; n++) {
+          if (el[method]) {
+            el[method](events[n], fns[i], options)
+          } else {
+            /* Fallback for older browsers IE <=8 */
+            el['on' + events[n]] = (isAdd) ? fns[i] : null
+          }
+        }
       }
+      // return opposite function with inverse event handler
+      return smartAttach.bind(null, !isAdd)
     }
-    // Fallback to attach/detach event. IE before version 9
-    /*
-    const ie9fallback = action[1] + EVENT
-    if (els[0][ie9fallback]) {
-      connectListener = fireAndReturn(() => {
-        return els.map((el) => {
-          const func = oncify(handler, opts)
-          events.forEach((evt) => el[ie9fallback]('on' + evt, func))
-          return func
-        })
-      })
-      // Return cleanup with re-initialize function
-      return function() {
-        els.forEach((el, i) => events.forEach((evt) => {
-          el[inverse[1] + EVENT]('on' + evt, connectListener.listeners[i])
-        }))
-        return connectListener.fn
-      }
-    }
-    */
-    /* Fallback for older browsers IE <=8 */
-    connectListener = fireAndReturn(() => {
-      return els.map((el) => {
-        events.forEach((evt) => el['on' + evt] = (add) ? oncify(handler, options) : null)
-      })
-    })
-    /* Return cleanup with re-initialize function */
-    return function() {
-      els.forEach((el) => {
-        events.forEach((evt) => el['on' + evt] = (add) ? null : oncify(handler, options))
-      })
-      return connectListener.fn
-    }
+
+    return smartAttach(add)
   }
 }
 
-function toArray(obj) {
-  // Is array, return it
-  if (Object.prototype.toString.call(obj) === '[object Array]') {
-    return obj
-  }
+function toArray(obj, isSelector) {
   // Split string
   if (isString(obj)) {
-    return obj.split(' ').map(e => e.trim())
+    return isSelector ? toArray(document.querySelectorAll(obj)) : obj.split(' ').map(e => e.trim())
   }
-  // Convert NodeList to array
+  // Convert NodeList to Array
   if (NodeList.prototype.isPrototypeOf(obj)) {
     const array = []
     for (var i = obj.length >>> 0; i--;) { // iterate backwards ensuring that length is an UInt32
@@ -92,17 +59,12 @@ function toArray(obj) {
     }
     return array
   }
-  // Convert single element to array
-  return [ obj ]
-}
-
-function isString(str) {
-  return typeof str === 'string'
+  // Is Array, return it OR Convert single element to Array
+  return isArray(obj) ? obj : [ obj ]
 }
 
 function oncify(handler, opts) {
-  if (opts && opts.once) return once(handler)
-  return handler
+  return (opts && opts.once) ? once(handler) : handler
 }
 
 function once(fn, context) {
@@ -116,15 +78,7 @@ function once(fn, context) {
   }
 }
 
-function fireAndReturn(fn) {
-  return { 
-    fn: fn, 
-    // attach and return
-    listeners: fn()
-  }
-}
-
-const addListener = createListener(true)
+const addListener = createListener(EVENT)
 const removeListener = createListener()
 
 export {
@@ -135,22 +89,3 @@ export {
   /* Listen to onChange events on 1 or more elements */
   removeListener,
 }
-
-/* Misc
-// return mapper(els, events, handler, options, (el, handle) => (event) => {
-//   el[method](event, handle, options)
-//   return handle
-// })
-// mapper(els, events, handler, options, (el) => (event, i) => {
-//   console.log(`disable ${event}`, inverse[0] + EventListener, el)
-//   console.log(connectListener.listeners[i])
-//   el[inverse[0] + EventListener](event, connectListener.listeners[i], options)
-// })
-*/
-// function mapper(elements, events, handler, opts, applier) {
-//   return elements.map((el) => {
-//     const handle = opts && opts.once ? once(handler) : handler
-//     events.forEach(applier(el, handle))
-//     return handle
-//   })
-// }
