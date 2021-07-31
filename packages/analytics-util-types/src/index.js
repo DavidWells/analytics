ad3faba5133e938ce0d958479abe90ddec48473e
David Wells <>
2021-07-30 23:57:36 -0700

    refactor: add types
    Signed-off-by: David Wells <>

--- packages/analytics-util-types/src/index.js
+++ packages/analytics-util-types/src/index.js
@@ -7,6 +7,8 @@ export const STRING = 'string'
 export const UNDEFINED = 'undefined'
 export const BOOLEAN = 'boolean'
 export const OBJECT = 'object'
+export const ARRAY = 'array'
+export const noOp = () => {}
 
 export const isBrowser = typeof window !== UNDEFINED
 
@@ -48,7 +50,7 @@ export function isBoolean(x) {
  * @return {x is Array<T>}
  */
 export function isArray(x) {
-  return Array.isArray(x)
+  return Object.prototype.toString.call(x) === '[object Array]'
 }
 
 /** 
@@ -64,4 +66,47 @@ export function isObject(obj) {
   }
 
   return Object.getPrototypeOf(obj) === proto
+}
+
+/** 
+ * @param func - function to check if noOp
+ * @return {Boolean} - is noOp
+ * @examples
+   function foo() {}
+   isNoOp(foo) // true
+   isNoOp(() => { }) // true
+   isNoOp(() => { console.log('hi') }) // false
+ */
+export function isNoOp(func) {
+  if (!isFunction(func)) return false
+  const emptyFunc = /{(\r|\n|\s)*}/gm
+  const noOpStr = noOp.toString()
+  const funcString = (func.toString().match(emptyFunc) || [''])[0].replace(emptyFunc, noOpStr)
+  return noOpStr === funcString
+}
+
+/** 
+ * @param obj
+ * @return {obj is NodeList}
+ */
+export function isNodeList(obj) {
+  return NodeList.prototype.isPrototypeOf(obj)
+}
+
+/**
+ * Check if element is form element
+ * @param {HTMLElement} element
+ * @return {boolean} 
+ */
+export function isForm(element) {
+  return isElement(element) && element.nodeName === 'FORM'
+}
+
+/**
+ * Check if element is form element
+ * @param {HTMLElement|*} element
+ * @return {boolean} 
+ */
+export function isElement(element) {
+  return element instanceof Element || element instanceof HTMLDocument
 }
