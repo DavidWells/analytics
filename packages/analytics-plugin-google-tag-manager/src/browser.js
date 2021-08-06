540d76bdf1a028c69ffda1cdb65438854dab20f5
Black-Hole <158blackhole@gmail.com>
2021-08-06 20:00:24 +0800

    fix(vulnerabilitie): prototype chain contamination vulnerability
    POC: https://github.com/BlackFan/client-side-prototype-pollution/blob/master//gadgets/analytics-google-tag-manager.md

--- packages/analytics-plugin-google-tag-manager/src/browser.js
+++ packages/analytics-plugin-google-tag-manager/src/browser.js
@@ -5,6 +5,7 @@ export const config = {
   dataLayer: undefined,
   preview: undefined,
   auth: undefined,
+  customScriptSrc: 'https://www.googletagmanager.com/gtm.js',
   // assumesPageview: true,
 }
 
@@ -44,8 +45,6 @@ function googleTagManager(pluginConfig = {}) {
         throw new Error('When enabling preview mode, both preview and auth parameters must be defined');
       }
 
-      const scriptSrc = customScriptSrc || 'https://www.googletagmanager.com/gtm.js';
-
       if (!scriptLoaded(containerId)) {
         /* eslint-disable */
         (function(w, d, s, l, i) {
@@ -56,7 +55,7 @@ function googleTagManager(pluginConfig = {}) {
             dl = l != 'dataLayer' ? '&l=' + l : '',
             p = preview ? '&gtm_preview=' + preview + '&gtm_auth=' + auth + '&gtm_cookies_win=x' : '';
           j.async = true;
-          j.src = `${scriptSrc}?id=` + i + dl + p;
+          j.src = `${customScriptSrc}?id=` + i + dl + p;
           f.parentNode.insertBefore(j, f);
         })(window, document, 'script', dataLayerName, containerId);
         /* eslint-enable */
