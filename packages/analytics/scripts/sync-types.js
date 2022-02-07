08f522a9f28698260ce09c239c29e4e56f131922
David Wells <>
2022-02-06 23:13:48 -0800

    chore: update type sync
    Signed-off-by: David Wells <>

--- packages/analytics/scripts/sync-types.js
+++ packages/analytics/scripts/sync-types.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 // Copy types from analytics core
-const SRC = path.resolve(__dirname, '../../analytics-core/lib/types.d.ts')
+const SRC = path.resolve(__dirname, '../../analytics-core/dist/types.d.ts')
 const DEST = path.resolve(__dirname, '../lib/types.d.ts')
 
 fs.copyFile(SRC, DEST, (err) => {
